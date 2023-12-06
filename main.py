39adbca47289f29b8ed84ddff1726511c2272ce2
Satyam Sharma <55931484+codedestructed007@users.noreply.github.com>
2023-12-07 02:31:35 +0530

    Added model training

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.cnnClassifier import logger
 from src.cnnClassifier.pipeline.data_ingestion_pipeline import STAGE_NAME, DataIngestionTrainingPipeline
 from src.cnnClassifier.pipeline.prepare_model_pipeline import STAGE_MODEL_PREPARATION , BaseModelPreparationPipeline
+from src.cnnClassifier.pipeline.model_training_pipeline import Stage_training, ModelTrainingPipeline
 
 
 try:
@@ -19,3 +20,16 @@ try:
     logger.info(f" >> Stage - {STAGE_MODEL_PREPARATION} completed successfully >>")
 except Exception as e:
     raise e
+
+
+
+try:
+    logger.info(f'>>>>>')
+    logger.info(f">>> stage {Stage_training} started>>>")
+    obj = ModelTrainingPipeline()
+    obj.main()
+    logger.info(f'>>> Stage {Stage_training} completed >>>>')
+except Exception as e:
+    raise e
+        
+            
---
+++ src/cnnClassifier/pipeline/model_training_pipeline.py
@@ -0,0 +1,28 @@
+import sys
+sys.path.append(r'c:\\Users\\Dell\\OneDrive\\Desktop\\Real_projects\\Kidney-Tumor_classification_Deep_learning')
+
+
+from src.cnnClassifier import logger
+from src.cnnClassifier.config.configuration import ConfigurationManager
+from src.cnnClassifier.components.model_training import Training
+
+
+Stage_training = 'Training'
+
+class ModelTrainingPipeline:
+    def __init__(self):
+        pass
+    
+    def main(self):
+        try:
+            config = ConfigurationManager()
+            training_model_config =config.get_ṭraining_model_config()
+            training = Training(training_model_config)
+            training.get_base_model()
+            training.train_valid_generator()
+            training.train()
+            
+        except Exception as e:
+            raise e
+            
+            
